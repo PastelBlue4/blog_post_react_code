62db6bb8ca53e41a52ec8731cd45106634a5aeed
Romuru <pastelblue0721@gmail.com>
2022-12-16 21:59:28 +0900

    feat(modal) new folder, useRef, modal window on/off :)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 
 import PropsLayout from "./1.Props/PropsLayout";
 import { TodoLogicTest } from "./2.Array/TodoLogicTest";
+import Modal from "./3.Modal /Modal";
 
 function App() {
   return (
     <>
-      <TodoLogicTest />
+      <Modal />
     </>
   );
 }
