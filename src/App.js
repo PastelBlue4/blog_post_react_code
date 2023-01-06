6efcf9a028e181b50c87a004ffc001ba950de781
Romuru <pastelblue0721@gmail.com>
2023-01-07 02:06:40 +0900

    feat modal example code done :) before styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 
-import PropsLayout from "./1.Props/PropsLayout";
-import { TodoLogicTest } from "./2.Array/TodoLogicTest";
 import Modal from "./3.Modal /Modal";
+import { Reset } from "styled-reset";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Reset />
+      <Modal />
+    </>
+  );
 }
 
 export default App;
